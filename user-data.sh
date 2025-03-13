7dc8402bfe868989d21ac6d57bc3ed5eea4f75cf
Frank Spiess <8014314+typex1@users.noreply.github.com>
2025-03-13 13:57:21 +0100

    Delete user-data.sh

--- user-data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# to be used as Amazon EC2 user data or run manually:
-sudo yum update -y
-sudo yum install git -y
-sudo yum install tree -y
-su - ec2-user -c 'wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py'
